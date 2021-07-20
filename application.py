3ccb4fd0a5e74997b2d4a5c14a795a72136a6bf1
Alex <alexcheung880@gmail.com>
2021-07-19 18:38:24 -0700

    using layout template now

--- application.py
+++ application.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+# basic route, index 
+# default method is GET
+@app.route('/') 
 def index():
-    return render_template("index.html", name=request.args.get("name"))
+    # request.args for get requests
+    return render_template("index.html", name=request.args.get("name", "world"))
+
+# greeting route
+@app.route("/greet", methods=["POST"])
+def greet():
+    # request.form for post requests
+    return render_template("greet.html", name=request.form.get("name", "there"))
 
 
 if __name__ == "__application__":
