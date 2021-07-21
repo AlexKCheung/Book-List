d4168a66f2d0c2f4673c34ca8475d7ad7cc1c3f1
Alex <alexcheung880@gmail.com>
2021-07-20 17:45:57 -0700

    bug fix if user tries to be sneaky

--- application.py
+++ application.py
@@ -27,7 +27,7 @@ def book_add():
     if request.method == "GET":
         return render_template("book_add.html", book_status=BOOK_STATUS)
     # else POST
-    if not request.form.get("book_title") or not request.form.get("book_status"):
+    if not request.form.get("book_title") or request.form.get("book_status") not in BOOK_STATUS:
         return render_template("book_failure.html")
     # else
     return render_template("book_success.html")
